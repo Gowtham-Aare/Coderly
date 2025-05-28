4d7b94fa7913471d2f317b7dfff02f68d97ca7fd
Gowtham-Aare <gowthamaare123@gmail.com>
2025-05-28 15:51:52 +0530

    src
    src  file

---
+++ src/app/pricing/_components/UpgradeButton.tsx
@@ -0,0 +1,19 @@
+import { Zap } from "lucide-react";
+import Link from "next/link";
+
+export default function UpgradeButton() {
+  const CHEKOUT_URL =
+    "https://coderly.lemonsqueezy.com/buy/7b3ca73a-1928-4dca-977b-686fb3c19f5d";
+
+  return (
+    <Link
+      href={CHEKOUT_URL}
+      className="inline-flex items-center justify-center gap-2 px-8 py-4 text-white 
+        bg-gradient-to-r from-blue-500 to-blue-600 rounded-lg 
+        hover:from-blue-600 hover:to-blue-700 transition-all"
+    >
+      <Zap className="w-5 h-5" />
+      Upgrade to Pro
+    </Link>
+  );
+}
